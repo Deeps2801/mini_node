36a66a57f8f3867deeae087fcd2b790e532eb7be
deepika <deepika.kamble@techno.soft.in>
2025-07-08 13:34:28 +0530

    update

--- index.js
+++ index.js
@@ -6,19 +6,29 @@ app.use(express.json()) // res.json(req.body) use here
 app.get("/" , (req, res)=>{
     res.json([{
         id : "1",
-        "name" : "Deepika",
-        "age" : 34
+        name : "Deepika",
+        age : 34,
+        city : "panvel"
+
     },
     {
         id : "2",
         name : "Priyanka",
-        age : 30
+        age : 30,
+        city : "new panvel"
     },
     {
         id : "3",
         name : "Nirmala",
-        age : 35
+        age : 35,
+        city : "Mankhurd"
     },
+    {
+        id : "4",
+        name : "Bhagyashree",
+        age : 33,
+        city : "Badlapur"
+    }
 
 
 ]);
